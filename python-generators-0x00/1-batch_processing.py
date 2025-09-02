48f5225ce7da5bf87fad5a1e8363b65bad39d3e3
AudiaOlwa <audiaolwa@gmail.com>
2025-09-02 14:06:55 +0100

    generator that fetch and process data in batches from the users database

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -23,9 +23,12 @@ def stream_users_in_batches(batch_size: int):
 
     conn.close()
 
+    
 def batch_processing(batch_size):
-    """Process batches: filter users age > 25 using yield"""
+    """Process batches: filter users age > 25"""
+    result = []
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user["age"] > 25:
-                yield user
+                result.append(user)
+    return result
