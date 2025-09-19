7fe11d47e4e0efa2e42164be8d0b5acc0b5d140e
AudiaOlwa <audiaolwa@gmail.com>
2025-09-19 22:25:45 +0100

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-
-#Unit tests for utils.access_nested_map
-
+"""
+Unit tests for the utils module
+Covers access_nested_map, get_json, and memoize
+"""
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
-from utils import access_nested_map
 from utils import access_nested_map, get_json, memoize
 
 
 
 class TestAccessNestedMap(unittest.TestCase):
-   # """Unit tests for access_nested_map function"""
+    """Unit tests for access_nested_map function"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -19,7 +19,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-       # """Test that access_nested_map returns expected result"""
+        """Test that access_nested_map returns expected result"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
