cadce0655c4c8dfa55c43d9606bbeca666026193
AudiaOlwa <audiaolwa@gmail.com>
2025-09-21 15:23:01 +0100

    Integration test: fixtures

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import os
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 """
@@ -11,6 +12,7 @@ Covers:
 - get_json
 - memoize
 """
+
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
@@ -21,6 +23,7 @@ class TestAccessNestedMap(unittest.TestCase):
     """
     Unit tests for access_nested_map function
     """
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -49,6 +52,7 @@ class TestGetJson(unittest.TestCase):
     """
     Unit tests for get_json function
     """
+
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
