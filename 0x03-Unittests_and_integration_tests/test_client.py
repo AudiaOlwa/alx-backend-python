fc63c783b381d5ef29f0e810d3febc7ddfb1e7d8
AudiaOlwa <audiaolwa@gmail.com>
2025-09-21 14:40:00 +0100

    Integration test: fixtures

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -92,8 +92,16 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         result = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(result, expected)
-        
+
 #exercice8
+org_payload = {"repos_url": "https://api.github.com/orgs/test_org/repos"}
+repos_payload = [
+    {"name": "repo1", "license": {"key": "apache-2.0"}},
+    {"name": "repo2", "license": {"key": "mit"}}
+]
+expected_repos = ["repo1", "repo2"]
+apache2_repos = ["repo1"]
+
 class MockResponse:
     """Helper class to mock requests.Response with .json()"""
     def __init__(self, payload):
