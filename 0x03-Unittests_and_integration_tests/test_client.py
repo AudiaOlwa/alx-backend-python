57e515387568b097a11ffc123b2770ccbc3a0a7b
AudiaOlwa <audiaolwa@gmail.com>
2025-09-21 17:22:28 +0100

    TASK6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,12 +3,14 @@ import unittest
 from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
-#Task4
+
+
+    # Task4
 class TestGithubOrgClient(unittest.TestCase):
     """
     Test GithubOrgClient class.
     """
-
+    
     @parameterized.expand([
         ("google",),
         ("abc",)
@@ -23,18 +25,20 @@ class TestGithubOrgClient(unittest.TestCase):
         result = client.org
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, {"key": "value"})
-#TASK5
+    # TASK5
     def test_public_repos_url(self):
         """
         Test GithubOrgClient._public_repos_url property.
         """
         payload = {"repos_url": "https://api.github.com/orgs/test_org/repos"}
         client = GithubOrgClient("test_org")
-        with patch.object(GithubOrgClient, "org", new_callable=PropertyMock) as mock_org:
+        with patch.object(GithubOrgClient, "org", 
+            new_callable=PropertyMock,
+        ) as mock_org:
             mock_org.return_value = payload
             result = client._public_repos_url
             self.assertEqual(result, payload["repos_url"])
-#TASK6
+    # TASK6
     @patch("client.get_json")
     def test_public_repos(self, mock_get_json):
         """
