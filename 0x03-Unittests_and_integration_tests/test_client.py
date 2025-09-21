436b7b61fa03b633318f84f110574c3691ab334d
AudiaOlwa <audiaolwa@gmail.com>
2025-09-21 17:14:21 +0100

    TASK6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -40,19 +40,16 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         Test GithubOrgClient.public_repos method.
         """
-        # Payload que get_json renverra
+         # Payload que get_json renverra
         repo_payload = [{"name": "repo1"}, {"name": "repo2"}]
         mock_get_json.return_value = repo_payload
-
         # Valeur mockée pour _public_repos_url
         mock_url = "https://api.github.com/orgs/test_org/repos"
         client = GithubOrgClient("test_org")
-
         # Patch de la propriété _public_repos_url
         with patch.object(GithubOrgClient, "_public_repos_url", new_callable=PropertyMock) as mock_repos_url:
             mock_repos_url.return_value = mock_url
             result = client.public_repos()
-
             # Vérifications
             self.assertEqual(result, ["repo1", "repo2"])
             mock_repos_url.assert_called_once()
