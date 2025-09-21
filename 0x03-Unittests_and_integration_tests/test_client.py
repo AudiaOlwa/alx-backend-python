28dcefe3eb3670beb4738fd0be99ce024b1575c0
AudiaOlwa <audiaolwa@gmail.com>
2025-09-21 17:12:19 +0100

    TASK6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -25,10 +25,35 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, {"key": "value"})
 #TASK5
     def test_public_repos_url(self):
-        """Test GithubOrgClient._public_repos_url property."""
+        """
+        Test GithubOrgClient._public_repos_url property.
+        """
         payload = {"repos_url": "https://api.github.com/orgs/test_org/repos"}
         client = GithubOrgClient("test_org")
         with patch.object(GithubOrgClient, "org", new_callable=PropertyMock) as mock_org:
             mock_org.return_value = payload
             result = client._public_repos_url
             self.assertEqual(result, payload["repos_url"])
+#TASK6
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """
+        Test GithubOrgClient.public_repos method.
+        """
+        # Payload que get_json renverra
+        repo_payload = [{"name": "repo1"}, {"name": "repo2"}]
+        mock_get_json.return_value = repo_payload
+
+        # Valeur mockée pour _public_repos_url
+        mock_url = "https://api.github.com/orgs/test_org/repos"
+        client = GithubOrgClient("test_org")
+
+        # Patch de la propriété _public_repos_url
+        with patch.object(GithubOrgClient, "_public_repos_url", new_callable=PropertyMock) as mock_repos_url:
+            mock_repos_url.return_value = mock_url
+            result = client.public_repos()
+
+            # Vérifications
+            self.assertEqual(result, ["repo1", "repo2"])
+            mock_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with(mock_url)
